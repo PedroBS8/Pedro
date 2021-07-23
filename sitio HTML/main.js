a34534ced49ed833dfc3f9502a651aa16f6e60f7
PedroBS8 <87870992+PedroBS8@users.noreply.github.com>
2021-07-23 11:25:31 -0500

    Add files via upload

---
+++ sitio HTML/main.js
@@ -0,0 +1,13 @@
+document.querySelector(".boton").addEventListener("click",() =>{
+document.querySelector(".cosas").classList.toggle("show");
+
+});
+
+ScrollReveal().reveal('.cabeza');
+
+
+
+
+
+
+
